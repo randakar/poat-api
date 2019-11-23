e7bedeac59f0bd9cac37900c39032a4bc299c15b
Floris Kraak <randakar@gmail.com>
2019-11-23 15:26:10 +0100

    Stop referring to Jupiter annotations in the main application test
    This is not the time to use beta status test frameworks

--- src/test/java/org/kraaknet/poatapi/PoatApiApplicationTests.java
+++ src/test/java/org/kraaknet/poatapi/PoatApiApplicationTests.java
@@ -1,10 +1,10 @@
 package org.kraaknet.poatapi;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 @Slf4j
 @SpringBootTest
