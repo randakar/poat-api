e67d44bc8918e514778934c54db9627f183a5a2c
Floris Kraak <randakar@gmail.com>
2019-11-23 18:17:09 +0100

    Add the correct RequiredArgs constructor to CardReference

--- src/main/java/org/kraaknet/poatapi/backend/http/model/CardReference.java
+++ src/main/java/org/kraaknet/poatapi/backend/http/model/CardReference.java
@@ -15,16 +15,18 @@ package org.kraaknet.poatapi.backend.http.model;
 
 import lombok.Builder;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@RequiredArgsConstructor
 public class CardReference {
 
   @NonNull
   private final String id;
 
   @NonNull
-  private CardType type;
+  private final CardType type;
 }
 
