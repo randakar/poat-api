19979025f5aa195fc23aee77b7632caacc802908
Floris Kraak <randakar@gmail.com>
2019-11-23 14:13:07 +0100

    Add a simple integration testconfig
    Let's tie things together into a "real" rest assured test.
    Of course the application itself has no endpoints, so we don't have anything to test against yet..

---
+++ src/test/java/org/kraaknet/poatapi/IntegrationTestConfig.java
@@ -0,0 +1,13 @@
+package org.kraaknet.poatapi;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("integrationtest")
+@Configuration
+@ComponentScan(basePackages = {"org.kraaknet.poatapi"})
+@EnableAutoConfiguration
+public class IntegrationTestConfig {
+}
