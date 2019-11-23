f4ab910803d8a70225647fdcddb9eb42d6ceca88
Floris Kraak <randakar@gmail.com>
2019-11-23 18:51:44 +0100

    Implement basic controller
    Now we're getting somewhere. Let't try to get those tests up and running again.

--- src/main/java/org/kraaknet/poatapi/web/PoatController.java
+++ src/main/java/org/kraaknet/poatapi/web/PoatController.java
@@ -1,9 +1,30 @@
 package org.kraaknet.poatapi.web;
 
-import org.springframework.stereotype.Controller;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kraaknet.poatapi.service.PoatService;
+import org.kraaknet.poatapi.web.dto.PowerOfAttorneyReferenceDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/power-of-attorney/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PoatController {
 
+    @NonNull
+    private final PoatService service;
 
+
+    @GetMapping
+    public List<PowerOfAttorneyReferenceDTO> getPowerOfAttorneyReferences() {
+        log.debug("getPowerOfAttorneyReferences()");
+        return service.getAllPowerOfAttorneyReferences();
+    }
 }
