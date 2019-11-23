dbea967e3d16c0d7a939c28a423de3d8e974270d
Floris Kraak <randakar@gmail.com>
2019-11-23 13:47:20 +0100

    Initial project setup from Spring Boot Initializer
    Generated stuff. Usually I delete the useless bits, like that maven wrapper, but let's take our esteemed reviewer through this step by step.

---
+++ src/main/java/org/kraaknet/poatapi/PoatApiApplication.java
@@ -0,0 +1,13 @@
+package org.kraaknet.poatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kraaknet/poatapi/PoatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kraaknet.poatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
