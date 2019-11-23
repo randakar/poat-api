920f1efc5bd42d013bf54a23121620743f65de53
Floris Kraak <randakar@gmail.com>
2019-11-23 13:57:10 +0100

    Add an assertion and some logging to PoatApiApplicationTests
    Would be nice to write tests first, functionality second. Good old test-driven development.
    
    However, Spring Boot Initializer gave us a bunch of functionality out of the box, and the only test for that functionality tests if this thing starts or not.
    We're going to need more than that ..

--- src/test/java/org/kraaknet/poatapi/PoatApiApplicationTests.java
+++ src/test/java/org/kraaknet/poatapi/PoatApiApplicationTests.java
@@ -1,13 +1,19 @@
 package org.kraaknet.poatapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
 @SpringBootTest
 class PoatApiApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("If we got here, the application started successfully.");
+        assertTrue(true);
     }
 
 }
