ea4b9bd44db87d11cfc0b9ef5633355677d2099c
Floris Kraak <randakar@gmail.com>
2019-11-23 20:49:42 +0100

    Test basic list endpoint
    If this works we have all the scaffolding there and we can get to the fun stuff ;-)

--- src/test/java/org/kraaknet/poatapi/IntegrationTests.java
+++ src/test/java/org/kraaknet/poatapi/IntegrationTests.java
@@ -1,18 +1,26 @@
 package org.kraaknet.poatapi;
 
+import io.restassured.http.ContentType;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kraaknet.poatapi.integration.RestAssuredConfigurer;
+import org.kraaknet.poatapi.web.dto.PowerOfAttorneyReferenceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 import static junit.framework.TestCase.assertTrue;
 
 @Slf4j
@@ -37,9 +45,27 @@ public class IntegrationTests {
     }
 
     @Test
-    public void contextLoads() {
-        log.debug("contextLoads()");
+    public void whenApplicationStartedSuccessfullyThisRunsToo() {
+        log.info("If we got here, the application started successfully.");
         assertTrue(true);
+    }
+
+    @Test
+    public void whenRequestingAllThePowerGetAllThePowers() {
+
+        ParameterizedTypeReference<List<PowerOfAttorneyReferenceDTO>> typeReference = new ParameterizedTypeReference<>() {
+        };
+
+        List<PowerOfAttorneyReferenceDTO> response = given()
+                .when()
+                .get("/power-of-attorney/")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .extract().body().as(typeReference.getType());
+
+        log.debug("Response: {}", response);
+
     }
 
 }
