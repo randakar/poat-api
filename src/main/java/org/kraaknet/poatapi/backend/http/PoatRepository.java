de8c898c9af2556a59d136816aa7191d1ec1e50b
Floris Kraak <randakar@gmail.com>
2019-11-26 21:24:09 +0100

    Add a call to get the individual power of attorney object for each poa to the backend repo

--- src/main/java/org/kraaknet/poatapi/backend/http/PoatRepository.java
+++ src/main/java/org/kraaknet/poatapi/backend/http/PoatRepository.java
@@ -3,8 +3,10 @@ package org.kraaknet.poatapi.backend.http;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kraaknet.poatapi.backend.http.model.PowerOfAttorney;
 import org.kraaknet.poatapi.backend.http.model.PowerOfAttorneyReference;
 import org.kraaknet.poatapi.config.ApplicationConfigProperties;
+import org.kraaknet.poatapi.exceptions.ApiException;
 import org.kraaknet.poatapi.exceptions.BackendException;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -14,11 +16,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 import java.util.Set;
+
+import static java.lang.String.format;
 
 @Slf4j
 @Component
@@ -40,23 +45,38 @@ public class PoatRepository {
     private final HttpClientErrorStatusConverter statusConverter;
 
     public Set<PowerOfAttorneyReference> getAllPowerOfAttorneyReferences() {
+        String url = createBaseUriBuilder()
+                .pathSegment("power-of-attorneys")
+                .build(true).toUriString();
+
+        ParameterizedTypeReference<Set<PowerOfAttorneyReference>> reference = new ParameterizedTypeReference<>() {
+        };
+
         try {
-            String url = createBaseUriBuilder()
-                    .pathSegment("power-of-attorneys")
-                    .build(true).toUriString();
-
-            ParameterizedTypeReference<Set<PowerOfAttorneyReference>> reference = new ParameterizedTypeReference<>() {
-            };
-
             ResponseEntity<Set<PowerOfAttorneyReference>> result = restTemplate.exchange(url, HttpMethod.GET, entity, reference);
 
             log.debug("Result: {}", result);
             return Optional.ofNullable(result.getBody())
                     .orElseThrow(() -> new BackendException("Empty response while requesting the list of power of attorneys."));
         } catch (HttpClientErrorException | HttpServerErrorException e) {
-            log.debug("Exception while talking to {}: {}", SERVICE_NAME, e.getMessage());
-            HttpStatus status = e.getStatusCode();
-            throw statusConverter.convertHttpErrorStatus(status, SERVICE_NAME);
+            throw handleException(e);
+        }
+    }
+
+    public PowerOfAttorney getPowerOfAttorney(long id) {
+        String prefixedId = String.format("%04d", id);
+        String url = createBaseUriBuilder()
+                .pathSegment("power-of-attorneys")
+                .pathSegment(prefixedId)
+                .build(true).toUriString();
+        try {
+            ResponseEntity<PowerOfAttorney> result = restTemplate.exchange(url, HttpMethod.GET, entity, PowerOfAttorney.class);
+
+            log.debug("Result: {}", result);
+            return Optional.ofNullable(result.getBody())
+                    .orElseThrow(() -> new BackendException(format("Empty response while requesting power of attorney: %s", id)));
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw handleException(e);
         }
     }
 
@@ -65,5 +85,11 @@ public class PoatRepository {
                 .scheme("http")
                 .host(applicationConfigProperties.getBackendHost())
                 .port(applicationConfigProperties.getBackendPort());
+    }
+
+    private ApiException handleException(HttpStatusCodeException e) {
+        log.debug("Exception while talking to {}: {}", SERVICE_NAME, e.getMessage());
+        HttpStatus status = e.getStatusCode();
+        return statusConverter.convertHttpErrorStatus(status, SERVICE_NAME);
     }
 }
