58b47226a8a4139520ccf3ce3a18a105504b9ac3
Floris Kraak <randakar@gmail.com>
2019-11-26 21:25:25 +0100

    Implement basic view service
    Or at least the structure, we still need a mapper so right now this returns nothing.

--- src/main/java/org/kraaknet/poatapi/service/PoatService.java
+++ src/main/java/org/kraaknet/poatapi/service/PoatService.java
@@ -4,8 +4,10 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kraaknet.poatapi.backend.http.PoatRepository;
+import org.kraaknet.poatapi.backend.http.model.PowerOfAttorney;
 import org.kraaknet.poatapi.backend.http.model.PowerOfAttorneyReference;
 import org.kraaknet.poatapi.service.mappers.PowerOfAttorneyReferenceMapper;
+import org.kraaknet.poatapi.web.dto.PowerOfAttorneyDTO;
 import org.kraaknet.poatapi.web.dto.PowerOfAttorneyReferenceDTO;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @Service
@@ -29,7 +33,19 @@ public class PoatService {
         Set<PowerOfAttorneyReference> references = repository.getAllPowerOfAttorneyReferences();
         return references.stream()
                 .map(mapper::sourceToDestination)
-                .collect(Collectors.toList());
+                .collect(toList());
+    }
+
+    public List<PowerOfAttorneyDTO> getPowerOfAttorneyView() {
+        Set<PowerOfAttorneyReference> references = repository.getAllPowerOfAttorneyReferences();
+        log.debug("List of references: {}", references);
+        List<PowerOfAttorney> powers = references.stream()
+                .map(PowerOfAttorneyReference::getId)
+                .map(Long::valueOf)
+                .map(repository::getPowerOfAttorney)
+                .collect(toList());
+        log.debug("List of actual powers: {}", powers);
+        return Collections.emptyList();
     }
 
 }
--- src/main/java/org/kraaknet/poatapi/web/PoatController.java
+++ src/main/java/org/kraaknet/poatapi/web/PoatController.java
@@ -4,12 +4,14 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kraaknet.poatapi.service.PoatService;
+import org.kraaknet.poatapi.web.dto.PowerOfAttorneyDTO;
 import org.kraaknet.poatapi.web.dto.PowerOfAttorneyReferenceDTO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @Slf4j
@@ -21,10 +23,17 @@ public class PoatController {
     @NonNull
     private final PoatService service;
 
-
-    @GetMapping
+    @GetMapping("reference")
     public List<PowerOfAttorneyReferenceDTO> getPowerOfAttorneyReferences() {
         log.debug("getPowerOfAttorneyReferences()");
         return service.getAllPowerOfAttorneyReferences();
     }
+
+    @GetMapping("view")
+    public List<PowerOfAttorneyDTO> getPowerOfAttorneyView(Principal principal) {
+        log.debug("getPowerOfAttorneyView({})", principal);
+        return service.getPowerOfAttorneyView();
+    }
+
+
 }
